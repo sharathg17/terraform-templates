c59081b9399b16d24ca7ee610b79e31055a5fc36
Sharath <sharathg17@gmail.com>
2018-11-08 23:40:16 +0530

    update

---
+++ variables/terraform.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  access_key = "AKIAIP6WKV2ECB4TLKVA"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+
+variable "zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+resource "aws_instance" "example" {
+  count                 = 2
+  availability_zone     = "${var.zones[count.index]}"
+  ami                   = "ami-07585467"
+  instance_type         = "t2.micro"
+}
+
+
